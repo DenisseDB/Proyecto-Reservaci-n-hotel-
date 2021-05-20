7e7cd2afb8ddfcd8472cf3db49e3e539b442f298
Denisse Dominguez Bolaños <80787054+DenisseDB@users.noreply.github.com>
2021-05-20 16:42:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 * A01702603
 * 18/05/2021
 * version : 1
+Main, corrida de codigo
 */
 
 #include <iostream>
@@ -17,12 +18,12 @@ using namespace std;
 int main() {
   cout<<"\n"<< endl;
   cout << "Habitaciones Estandar ocupadas" << endl;
-  Estandar ivanna("WIFI gratuito", 2, 34, "Mar", "Baño sencillo", 3, 1); // crear nuevo objeto
-	ivanna.imprimeDatos();
+  Estandar ivanna("WIFI gratuito", 2, 34, "Mar", "Baño sencillo", 3, 1); // creacion de nuevo objeto, clase estandar con sobrecarga
+  ivanna.imprimeDatos();
   ivanna.imprimirPrecioTarifa();
   cout<<"\n"<< endl;
   cout << "Habitaciones Suite ocupadas" << endl;
-  Suite oscar("Room service", 5, "Jardin", "1-2 camas king size", "Albercas gold", 1, 4);
+  Suite oscar("Room service", 5, "Jardin", "1-2 camas king size", "Albercas gold", 1, 4);// creacion de nuevo objeto, clase suite con sobrecarga
   oscar.imprimeDatos();
   oscar.imprimirPrecioTarifa(); 
 }
